36d8407d91ec95070c9668d20edb60a1d13ac118
Mohammad ABDUL <abdul.opto@gmail.com>
2025-02-06 15:39:19 +0100

    Ajout des composants modifier et suppr

--- src/Pages/Doctor/Doctor.jsx
+++ src/Pages/Doctor/Doctor.jsx
@@ -1,29 +1,39 @@
 import './Doctor.css';
-import { useParams } from 'react-router-dom';
+import { useParams, NavLink } from 'react-router-dom';
 import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import DeleteBtn from '../../Components/DeleteBtn/DeleteBtn';
 
 const Doctor = () => {
   const { id } = useParams();
-  const [doctor, setDoctor] = useState([]);
+  const [doctor, setDoctor] = useState({
+    firstname: "",
+    lastname: "",
+    specialty: "",
+    city: "",
+    zip: "",
+    phone: "",
+    image: "",
+  });
 
   const getDoctorAsync = async () => {
-      try {
-        const response = await fetch(`https://localhost:8000/api/doctors/${id}`);
-        if (!response.ok) {
-          toast.error("Erreur lors de la recherche des informations du docteur.");
-        }
-        const data = await response.json();
-        console.log(data);
-        setDoctor(data); 
-      } catch (error) {
-        console.error(error);
-        toast.error("Impossible de se connecter pour récupérer les informations du docteur. Veuillez réessayer plus tard.");
+    try {
+      const response = await fetch(`https://localhost:8000/api/doctors/${id}`);
+      if (!response.ok) {
+        toast.error("Erreur lors de la recherche des informations du docteur.");
       }
-    };
+      const data = await response.json();
+      // console.log(data);
+      setDoctor(data); 
+    } catch (error) {
+      console.error(error);
+      toast.error("Impossible de se connecter pour récupérer les informations du docteur. Veuillez réessayer plus tard.");
+    }
+  };
 
-    useEffect(() => {
-      getDoctorAsync();
-    }, []);
+  useEffect(() => {
+    getDoctorAsync();
+  }, []);
 
   return (
     <div className="doctor-details">
@@ -41,6 +51,15 @@ const Doctor = () => {
             <p><strong>Téléphone:</strong> {doctor.phone}</p>
           </div>
         </div>
+        <NavLink
+          to={`/docteur/update/${doctor.id}`}
+          aria-label={`Aller à la page pour modifier les informations d'un docteur`}
+          className="update"
+        >
+          Modifier
+        </NavLink>
+
+        <DeleteBtn doctorId={doctor.id} />
       </div>
     </div>
   );
