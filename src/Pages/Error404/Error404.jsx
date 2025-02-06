35c4c6750a0c0bc02a743d55ab473cdaf8de5f5d
Mohammad ABDUL <abdul.opto@gmail.com>
2025-02-06 16:26:05 +0100

    page erreur 404 finie

--- src/Pages/Error404/Error404.jsx
+++ src/Pages/Error404/Error404.jsx
@@ -1,10 +1,14 @@
 import './Error404.css';
+import { Link } from "react-router-dom";
+
 
 const Error404 = () => {
   return (
-    <article id='Error404'>
-      
-    </article>
+    <article id="Error404">
+    <h1 className="bold">404</h1>
+    <h2>Oops, la page que vous chercher n'existe plus.</h2>
+    <Link to="/">Retourner à la page d'acceuil</Link>
+  </article>
   );
 };
 
